898019dbe5a9fd519b56f04ea9ae156aaa2ac213
dnzma13 <dnzma13@naver.com>
2022-08-03 17:58:34 +0900

    2022.08.03

--- src/com/day0803/BaekJoon1149_RGB.java
+++ src/com/day0803/BaekJoon1149_RGB.java
@@ -1,10 +1,47 @@
 package com.day0803;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BaekJoon1149_RGB {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		int[][] arr = new int[N][3];
+		int[] sum = new int[N];
+		int min;
+		int pre_idx = -1;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			min = Integer.MAX_VALUE;
+			for (int j = 0; j < 3; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				if (i == 0) {
+					
+				}
 
+//				if (min > arr[i][j]) {
+//					if (pre_idx == j && i != 0) { // 위의 집과 같은 색 일 때
+//						min = Math.min(sum[i - 2] + arr[i][j], sum[i - 1]);
+//						if (min == sum[i - 2] + arr[i][j]) {
+//							sum[i] = min;
+//						}
+//					} else { // 위의 집과 다른 색일 때
+//						if (i != 0) {
+//							min = sum[i - 1] + arr[i][j];
+//							pre_idx = j;
+//							sum[i] = min;
+//						}
+//					}
+//					if (i != 0)
+//						sum[i] = sum[i - 1] + min;
+//				}
+			}
+		}
+		System.out.println(sum[N - 1]);
 	}
-
 }
--- src/com/day0803/BaekJoon11660_Sum.java
+++ src/com/day0803/BaekJoon11660_Sum.java
@@ -3,6 +3,7 @@ package com.day0803;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BaekJoon11660_Sum {
@@ -13,22 +14,18 @@ public class BaekJoon11660_Sum {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		// 배열 초기화
-		int[][] arr = new int[N][N];
-		int[][] sum = new int[N][N];
-		for (int i = 0; i < N; i++) {
+		int[][] arr = new int[N + 1][N + 1];
+		int[][] sum = new int[N + 1][N + 1];
+		for (int i = 0; i < N + 1; i++) {
+			for (int j = 0; j < N + 1; j++) {
+				arr[i][0] = 0;
+				arr[0][j] = 0;
+			}
+		}
+		for (int i = 1; i < N + 1; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < N; j++) {
+			for (int j = 1; j < N + 1; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
-				if (i == 0 && j == 0) {
-					sum[i][j] = arr[i][j];
-					continue;
-				} else if (i == 0) {
-					sum[i][j] = sum[0][j - 1] + arr[i][j];
-					continue;
-				} else if (j == 0) {
-					sum[i][j] = sum[i - 1][0] + arr[i][j];
-					continue;
-				}
 				sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + arr[i][j];
 			}
 		}
@@ -36,25 +33,17 @@ public class BaekJoon11660_Sum {
 		int y1 = 0;
 		int x2 = 0;
 		int y2 = 0;
-		int diffX;
 		// 계산하기
 		for (int i = 0; i < M; i++) {
 			int result = 0;
 			// 인덱스 값 받기
 			st = new StringTokenizer(br.readLine(), " ");
-			x1 = Integer.parseInt(st.nextToken()) - 1;
-			y1 = Integer.parseInt(st.nextToken()) - 1;
-			x2 = Integer.parseInt(st.nextToken()) - 1;
-			y2 = Integer.parseInt(st.nextToken()) - 1;
-			if (x1 - 1 < 0 && y1 - 1 < 0) {
-				result = sum[0][0];
-			} else if (x1 - 1 < 0) {
-				result = sum[x2][y2] - sum[0][y2] - sum[x2][y1 - 1] + sum[0][y1 - 1];
-			} else if (y1 < 0) {
-				result = sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][0] + sum[x1 - 1][0];
-			} else {
-				result = sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1];
-			}
+			x1 = Integer.parseInt(st.nextToken());
+			y1 = Integer.parseInt(st.nextToken());
+			x2 = Integer.parseInt(st.nextToken());
+			y2 = Integer.parseInt(st.nextToken());
+
+			result = sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1];
 			System.out.println(result);
 		}
 	}
