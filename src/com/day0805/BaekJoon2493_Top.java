3a3c1339c2c9dd212994eff119355f0a50d9f7b8
dnzma13 <dnzma13@naver.com>
2022-08-05 18:50:34 +0900

    2022.08.05

--- src/com/day0805/BaekJoon2493_Top.java
+++ src/com/day0805/BaekJoon2493_Top.java
@@ -21,18 +21,25 @@ public class BaekJoon2493_Top {
 			int num = Integer.parseInt(st.nextToken());
 			if (numbers.isEmpty()) {
 				numbers.push(num);
-				idx.push(0);
-				idx.push(0);
+				idx.push(1);
 				continue;
 			}
 			if (numbers.peek() < num) {
-				numbers.pop();
-				idx.pop();
-				sb.append(" "+ idx.peek());
+				while (numbers.peek() < num) {
+					numbers.pop();
+					idx.pop();
+					if (numbers.isEmpty())
+						break;
+				}
+				if (numbers.isEmpty()) {
+					sb.append(" " + 0);
+				} else {
+					sb.append(" " + idx.peek());
+				}
 				numbers.push(num);
 				idx.push(i);
 			} else {
-				sb.append(" "+ idx.peek());
+				sb.append(" " + idx.peek());
 				numbers.push(num);
 				idx.push(i);
 			}
