1a041267756f8bee6e1ba68f3fb52e8cb19e542f
dnzma13 <dnzma13@naver.com>
2022-08-12 17:59:25 +0900

    2022.08.12

--- src/com/day0812/BaekJoon7568_Bulk.java
+++ src/com/day0812/BaekJoon7568_Bulk.java
@@ -3,20 +3,46 @@ package com.day0812;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+
+class Person {
+	int weight; // 무게
+	int height; // 키
+	int index; // 인덱스
+
+	Person(int weight, int height) {
+		this.weight = weight;
+		this.height = height;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [index=" + index + "]";
+	}
+}
 
 public class BaekJoon7568_Bulk {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		//배열 초기화
-		int[][] person = new int[n][2];
+		Person[] persons = new Person[n];
+		// 사람들의 몸무게, 키 정보를 담은 배열 초기화
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			person[i][0] = Integer.parseInt(st.nextToken());
-			person[i][1] = Integer.parseInt(st.nextToken());
+			persons[i] = new Person(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			persons[i].index = i;
 		}
-		
+		Arrays.sort(persons, (p1, p2) -> {
+			if (p1.weight > p2.weight && p1.height > p2.height) {
+				// 키와 몸무게 모두 크면 p2-p1
+				return p2.weight - p1.weight;
+			} else {
+				return 0;
+			}
+		});
+		StringBuilder sb = new StringBuilder();
+		System.out.println(Arrays.toString(persons));
 	}
 }
