85e099648f1d923adab05fa377ea73a63a43e97a
이현진 <dnzma13@naver.com>
2022-08-16 13:11:29 +0900

    2022.08.16

--- src/com/day0816/BaekJoon2839_Sugar.java
+++ src/com/day0816/BaekJoon2839_Sugar.java
@@ -9,21 +9,23 @@ public class BaekJoon2839_Sugar {
 		int weight = sc.nextInt();
 		int count = 0;
 		while (weight > 0) {
+			// 5의 배수가 아니라면
 			if (weight % 5 != 0) {
+				// 3보다 크거나 같다면 3빼기
 				if (weight >= 3) {
 					weight -= 3;
 					count++;
+				// 0보다 크고 3보다 작다면 만들 수 없는 경우이므로 -1로 바꾸고 반복 탈출
 				} else {
 					count = -1;
 					break;
 				}
+			// 5의 배수라면
 			} else {
+				// 5보다 크거나 같다면 5빼기
 				if (weight >= 5) {
 					weight -= 5;
 					count++;
-				} else {
-					count = -1;
-					break;
 				}
 			}
 		}
