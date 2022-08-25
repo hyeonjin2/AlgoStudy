3e93eaba2c3dbef9a1c938958eee4d7e8a251fdf
이현진 <dnzma13@naver.com>
2022-08-25 22:11:24 +0900

    2022.08.25

--- src/com/day0825/BaekJoon1753_ShortestPathAdjList.java
+++ src/com/day0825/BaekJoon1753_ShortestPathAdjList.java
@@ -21,15 +21,6 @@ public class BaekJoon1753_ShortestPathAdjList {
 		}
 	}
 
-	static class Vertex {
-		int no, weight;
-
-		public Vertex(int no, int weight) {
-			this.no = no;
-			this.weight = weight;
-		}
-	}
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -48,70 +39,45 @@ public class BaekJoon1753_ShortestPathAdjList {
 
 			adjList[from] = new Node(to, weight, adjList[from]);
 		}
-		int cnt = 0;
-		int[] result = new int[V + 1];
 		int[] D = new int[V + 1];
-		Arrays.fill(result, Integer.MAX_VALUE);
 		boolean[] visited = new boolean[V + 1];
 		Arrays.fill(D, Integer.MAX_VALUE);
-		PriorityQueue<Vertex> queue = new PriorityQueue<>((v1, v2) -> v1.weight - v2.weight);
-		queue.offer(new Vertex(start, 0));
+
 		D[start] = 0;
-//		System.out.println("============" + end + "=============");
-		// step1. 미방문 정점 중 출발지에서 자신으로의 비용이 최소인 정점 선택
-		// 방문해야하는 나머지 정점 중 출발지에서 가장 가까운 정점 찾기
-		while (!queue.isEmpty()) {
-			cnt++;
-			Vertex minVertex = queue.poll();
-//			System.out.println(minVertex.no + " " + D[minVertex.no]);
-			// 이미 방문했던 정점이면 최소값이 아님 -> 건너뛰기
-			if (visited[minVertex.no])
+		int min, minVertex;
+		for (int i = 0; i < V; i++) {
+			min = Integer.MAX_VALUE;
+			minVertex = -1;
+			// step1. 미방문 정점 중 출발지에서 자신으로의 비용이 최소인 정점 선택
+			// 방문해야하는 나머지 정점 중 출발지에서 가장 가까운 정점 찾기
+			for (int j = 1; j <= V; j++) {
+				if (!visited[j] && min > D[j]) {
+					min = D[j];
+					minVertex = j;
+				}
+			}
+			if (minVertex == -1) {
 				continue;
+			}
 			// step2. 방문처리
-			visited[minVertex.no] = true;
-			result[minVertex.no] = D[minVertex.no];
+			visited[minVertex] = true;
 
 			// step3. 선택된 정점을 경유지로 해서 미방문 정점들로 가는 비용을 따져보고 기존 최적해보다 유리하면 갱신
-			for (Node temp = adjList[minVertex.no]; temp != null; temp = temp.next) {
-				if (!visited[temp.vertex] && D[temp.vertex] > D[minVertex.no] + temp.weight) {
-					D[temp.vertex] = D[minVertex.no] + temp.weight;
-					queue.offer(new Vertex(temp.vertex, D[temp.vertex]));
+			for (Node temp = adjList[minVertex]; temp != null; temp = temp.next) {
+				if (!visited[temp.vertex] && D[temp.vertex] > D[minVertex] + temp.weight) {
+					D[temp.vertex] = D[minVertex] + temp.weight;
 				}
-
 			}
 		}
 		StringBuilder sb = new StringBuilder();
 		for (int i = 1; i <= V; i++) {
-			if (result[i] == Integer.MAX_VALUE) {
+			if (!visited[i]) {
 				sb.append("INF").append("\n");
 				continue;
 			}
-			sb.append(result[i]).append("\n");
+			sb.append(D[i]).append("\n");
 		}
 		System.out.println(sb);
 //		System.out.println("cnt : " + cnt);
 	}
 }
-/*
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- */
